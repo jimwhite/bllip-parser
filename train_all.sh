c27e8ee5087db53cdf3183d08e59192b883a5d3f
Jim White <james.paul.white@gmail.com>
2013-04-28 00:53:58 -0700

    Train using only per-parser data.

--- train_all.sh
+++ train_all.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-BROWNTREEBANK=xtrain
-
-if [ -d "$BROWNTREEBANK" ]; then
-
-echo "Doing self-training."
+PARSER_TRAINING_DIR=xtrain
+PARSER_TUNING_DIR=xtune
 
 PENNWSJTREEBANK=/corpora/LDC/LDC99T42/RAW/parsed/mrg/wsj
+
+if [ -d "$PARSER_TRAINING_DIR" ]; then
+
+echo "Training with per-parser data."
 
 mkdir tmp
 
 #XTRAIN=`echo $PENNWSJTREEBANK/0[2-9]/*mrg $PENNWSJTREEBANK/1[0-9]/*mrg $PENNWSJTREEBANK/2[0-1]/*mrg $BROWNTREEBANK/*01.mrg $BROWNTREEBANK/*[1-9][0-9].mrg $BROWNTREEBANK/*0[3-9].mrg`
-XTRAIN=`echo $PENNWSJTREEBANK/0[2-9]/*mrg $PENNWSJTREEBANK/1[0-9]/*mrg $PENNWSJTREEBANK/2[0-1]/*mrg $BROWNTREEBANK/*.mrg`
+#XTRAIN=`echo $PENNWSJTREEBANK/0[2-9]/*mrg $PENNWSJTREEBANK/1[0-9]/*mrg $PENNWSJTREEBANK/2[0-1]/*mrg $BROWNTREEBANK/*.mrg`
+
+XTRAIN=`echo $PARSER_TRAINING_DIR/*.mrg`
 echo $XTRAIN >tmp/train-all-files.txt
 cat $XTRAIN >tmp/train-all.mrg 
 
+
+if [ -d "$PARSER_TUNING_DIR" ]; then
+XDEV=`echo $PARSER_TUNING_DIR/*.mrg`
+else
 #XDEV=`echo $BROWNTREEBANK/*02.mrg`
 XDEV=`echo $PENNWSJTREEBANK/24/*.mrg`
+fi
+
 echo $XDEV >tmp/dev-all-files.txt
 cat $XDEV >tmp/dev-all.mrg
 
